47780b3a45ff38a0be63d888268728472a79799d
SARATHI <142735146+SARATHI-183@users.noreply.github.com>
2024-09-09 14:55:29 +0530

    Update Constant.jsx

--- src/Data/Constant.jsx
+++ src/Data/Constant.jsx
@@ -9,7 +9,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/SARATHI-183",
     resume:
-      "https://drive.google.com/file/d/1CP10L19jAwN2X0iq2foh2CRfJlwcKg9Y/view?usp=drivesdk",
+      "https://drive.google.com/file/d/1waLI_kLt8RJ5LAw3O5VhEBw6EG4A7X7p/view?usp=drivesdk",
     linkedin: "https://www.linkedin.com/in/sarathi-p-78b980245/",
     twitter: "",
     insta: "https://www.instagram.com/_.sarathi__/",

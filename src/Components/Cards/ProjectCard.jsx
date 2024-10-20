557bf29f1c2b2955a9b1af6e2f3ffc0b3a6a1503
SARATHI-183 <sarathip.21cse@kongu.edu>
2024-10-20 19:33:12 +0530

    styles added

--- src/Components/Cards/ProjectCard.jsx
+++ src/Components/Cards/ProjectCard.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { FaLink } from "react-icons/fa";
 
 const ProjectCard = ({project}) => {
+        const handleCardClick = (githubLink) => {
+          window.open(githubLink, '_blank');  // Opens the GitHub link in a new tab
+        };
+
+
   return (
     <div className='w-[280px] h-[490px] bg-[#171721] cursor-pointer rounded-[10px] 
     shadow-[0px_0px_12px_4px_rgba(0,0,0,0.4)] overflow-hidden transition-all delay-200 
     ease-in-out hover:translate-y-[-10px] hover:shadow-[0px_0px_50px_4px_rgba(0,0,0,0.6)] 
-    hover:brightness-110    flex flex-col px-[20px] py-[26px] gap-[14px] '>
+    hover:brightness-110    flex flex-col px-[20px] py-[26px] gap-[14px] ' 
+    onClick={()=> handleCardClick(project.github)}>
         <img src={project.image} className='w-full h-[180px] rounded-[10px] object-cover bg-[#FFFFFF] shadow-[0px_0px_16px_2px_rgba(0,0,0,0.3)]'></img>
         <div className='w-full flex flex-wrap mt-[4px] items-center gap-[8px] '>
             {project.tags.map((tag)=>(
@@ -24,7 +30,7 @@ const ProjectCard = ({project}) => {
             <div className='flex items-center group gap-2'>
                 <FaLink   className='text-[#b1b2b399] font-normal w-[20px] h-[20px] items-center mt-[8px] group-hover:text-[#854CE6]' />
                 <a className='text-[#b1b2b399]  font-normal overflow-hidden text-ellipsis line-clamp-1 mt-[8px] max-w-full group-hover:text-[#854CE6]' href={project.webapp || project.github} target='_blank'>
-                    {project.webapp || project.github}
+                    {project.webapp }
                 </a>
            </div>
         </div>

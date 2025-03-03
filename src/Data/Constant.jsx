0daa861c3abee3e20896ac29beb517de3f960da4
Your Name <your-email@example.com>
2025-03-03 21:40:31 +0530

    added updated resume

--- src/Data/Constant.jsx
+++ src/Data/Constant.jsx
@@ -9,7 +9,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/SARATHI-183",
     resume:
-      "https://drive.google.com/file/d/1AP6pdryQyzfFdTtSXCI9hGxRiqW9laR5/view?usp=sharing",
+      "https://drive.google.com/file/d/11xR5p7fJklEHn50GOBx6n6lZ-Ogo7TRH/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/sarathi-p-78b980245/",
     twitter: "",
     insta: "https://www.instagram.com/_.sarathi__/",
